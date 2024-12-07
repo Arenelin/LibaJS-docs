05018c063065a04d2fa2933f24d3ce5627540b0f
nakmaykin <nakmaykin@sfxdx.com>
2024-12-07 09:33:52 +0700

    feat: adds initial setup and documentation display

---
+++ docs/introduction/installation.md
@@ -0,0 +1,14 @@
+---
+sidebar_position: 1
+---
+
+# Installation
+
+LibaJS is available as an NPM package for use with a module wrapper or in a Node application:
+```shell
+# NPM
+npm install liba-js
+
+# Yarn
+yarn add liba-js
+```
--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,7 +5,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'My Site',
+  title: 'LibaJS',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
@@ -55,7 +55,7 @@ const config: Config = {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
-      title: 'My Site',
+      title: 'LibaJS',
       logo: {
         alt: 'My Site Logo',
         src: 'img/logo.svg',
@@ -68,7 +68,7 @@ const config: Config = {
           label: 'Tutorial',
         },
         {
-          href: 'https://github.com/facebook/docusaurus',
+          href: 'https://github.com/Arenelin/LibaJS',
           label: 'GitHub',
           position: 'right',
         },
@@ -90,21 +90,13 @@ const config: Config = {
           title: 'Community',
           items: [
             {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'X',
-              href: 'https://x.com/docusaurus',
+              label: 'Telegram',
+              href: 'https://t.me/Arenelin',
             },
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} LibaJS.`,
     },
     prism: {
       theme: prismThemes.github,
